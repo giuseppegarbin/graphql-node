6f5fdc24037e391535db69826c9cc2790297b301
Garbin <garbin@ad.garmin.com>
2018-01-23 14:56:02 +0100

    ApolloClient succesfully created

--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Create ApolloClient and Provider
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: process.env.REACT_APP_ENPOINT_URI }),
+    cache: new InMemoryCache()
+});
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
